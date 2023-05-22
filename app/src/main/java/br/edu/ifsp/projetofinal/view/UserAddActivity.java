e3542b4f5e116b571477cb3c84d8248a2a53046f
fernandaagustoni <fernanda_agustoni@hotmail.com>
2023-05-22 10:59:42 -0300

    tela cadastro

---
+++ app/src/main/java/br/edu/ifsp/projetofinal/view/UserAddActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.projetofinal.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import br.edu.ifsp.projetofinal.R;
+
+public class UserAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_add);
+    }
+}
